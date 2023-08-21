5969795d5932d11bcd1ac2c3d0cb60a87312f163
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:32:44 +0300

    valid-parenthesis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,22 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 export function isValid(str) {
+  const pairs = {
+    ')' : '(',
+    '}' : '{',
+    ']' : '[',
+  }
+  const stack = [];
 
+  for (let i = 0; i < str.length; i++) {
+
+    const char = str[i];
+    if (!pairs[char]) {
+      stack.push(char);
+    } else if (pairs[char] !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return true;
 }
