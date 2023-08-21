de3eb8a25300a7e9c4dc3a4e88c0edcff9c1689d
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:21:14 +0300

    dfs

--- problems/depth-first-search/depth-first-search.js
+++ problems/depth-first-search/depth-first-search.js
@@ -11,5 +11,17 @@
  */
 
 export function dfs(tree) {
+  const result = [];
+  const stack = [];
 
+  stack.push(tree);
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+
+  return result;
 }
