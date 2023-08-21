3e9a9ca795f457a38a6dd9c957594922b6dbef52
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 21:36:58 +0300

    max-subarray

--- problems/maximum-subarray/maximum-subarray.js
+++ problems/maximum-subarray/maximum-subarray.js
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/maximum-subarray/
 
 export function maximumSubarray(arr) {
+  let currMax = arr[0];
+  let currSum = arr[0];
 
+  for (let i = 1; i < arr.length; i++) {
+    currSum = Math.max(arr[i], currSum + arr[i]);
+    currMax = Math.max(currMax, currSum);
+  }
+
+  return currMax;
+  
 }
