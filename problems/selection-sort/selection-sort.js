c8c8cd3b6359b734d207a669dd89236fe32a52ab
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 18:50:27 +0300

    selection-sort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -2,6 +2,16 @@
 // https://en.wikipedia.org/wiki/Selection_sort
 
 export function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let index;
+        let min = Infinity; 
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                index = j;
+            }
+        }
+        [arr[index], arr[i]] = [arr[i], arr[index]];
+    }
     return arr;
 }
