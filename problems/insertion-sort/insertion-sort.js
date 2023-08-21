cd2b54d8a39389ce4d1e3e427349051b74cb0293
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:05:06 +0300

    fix: j > 0 check

--- problems/insertion-sort/insertion-sort.js
+++ problems/insertion-sort/insertion-sort.js
@@ -4,7 +4,7 @@
 export function insertionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let j = i;
-        while (arr[j] < arr[j-1]) {
+        while ((j > 0) && (arr[j] < arr[j-1])) {
             [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
             j--;
         }
