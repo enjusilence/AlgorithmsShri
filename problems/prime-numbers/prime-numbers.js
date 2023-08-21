fab39573ebcb04ffe017aecb7a4a69dc5c99ad3a
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:51:44 +0300

    prime-numbers

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -2,6 +2,17 @@
 
 export function primeNumbers(num) {
     const result = [];
+    let array = (Array.from({length: num + 1}, (_, i) => i)).slice(2);
+    const maxNumToCheck = Math.ceil(Math.sqrt(num));
+    console.log(maxNumToCheck)
+
+    while (array.length > 0) {
+        const curr = array.shift();
+        result.push(curr);
+        if (curr <= maxNumToCheck) {
+            array = array.filter(x => (x % curr));
+        }
+    }
 
     return result;
 }
