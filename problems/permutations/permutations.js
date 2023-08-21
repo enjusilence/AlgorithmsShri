2a2d8aeef567928d11f65e0d293bc6b83ff3face
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 21:20:58 +0300

    permutations

--- problems/permutations/permutations.js
+++ problems/permutations/permutations.js
@@ -2,6 +2,21 @@
 
 export function permutations(n) {
     const result = [];
+    const nums = Array.from({length: n + 1}, (_, i) => i).slice(1);
+
+    recursiveIteration([], nums)
+
+    function recursiveIteration(currPerm, nums) {
+        if (currPerm.length === n) {
+            result.push(currPerm);
+        } else {
+            for (let i = 0; i < nums.length; i++) {
+                const nextNums = [...nums];
+                const [nextNum] = nextNums.splice(i, 1);
+                recursiveIteration([...currPerm, nextNum], nextNums);
+            }
+        }
+    }
 
     return result;
 }
