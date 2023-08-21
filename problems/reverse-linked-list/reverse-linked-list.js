f54b6c839f29d0012e3fac182122687618df6b31
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 21:02:37 +0300

    reverse-linked-list

--- problems/reverse-linked-list/reverse-linked-list.js
+++ problems/reverse-linked-list/reverse-linked-list.js
@@ -9,5 +9,17 @@
  */
 
 export function reverseList(head) {
+  let endNode;
 
+  reverseNode(head);
+
+  function reverseNode(node, parent) {
+    if (node.next) {
+      reverseNode(node.next, node)
+    } else {
+      endNode = node;
+    }
+    node.next = parent;
+  }
+  return endNode;
 }
