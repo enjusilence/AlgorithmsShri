a7a96f892c905f574a16e281f3c971dd03321a0a
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 18:36:57 +0300

    bubble-sort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -2,6 +2,15 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 export function bubbleSort(arr) {
-
+    let hasChanged = true;
+    while (hasChanged) {
+        hasChanged = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i+1]) {
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                hasChanged = true;
+            }
+        }
+    }
     return arr;
 }
