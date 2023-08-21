6b9def20f575aa19cf97ba5bcf4d927ba44e687e
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:23:48 +0300

    bfs

--- problems/breadth-first-search/breadth-first-search.js
+++ problems/breadth-first-search/breadth-first-search.js
@@ -11,6 +11,16 @@
 
 export function bfs(tree) {
     const result = [];
+    const queue = [];
+
+    queue.push(tree);
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+        result.push(node.val);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
 
     return result;
 }
