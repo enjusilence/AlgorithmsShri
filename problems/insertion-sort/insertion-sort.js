0e4c9b523e0ff967d4d90c693511b7d538f06916
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 19:00:10 +0300

    insertion-sort

--- problems/insertion-sort/insertion-sort.js
+++ problems/insertion-sort/insertion-sort.js
@@ -2,6 +2,12 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 export function insertionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let j = i;
+        while (arr[j] < arr[j-1]) {
+            [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+            j--;
+        }
+    }
     return arr;
 }
