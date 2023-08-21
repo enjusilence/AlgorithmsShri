1641513eebead345abd3da5af1db08abb807fc24
Vladislav Salnikov <vssalnikov95@gmail.com>
2023-08-21 20:47:36 +0300

    binary-search

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,17 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
+  let [l, r] = [0, arr.length - 1];
 
+  while (l < r) {
+    const i = Math.ceil((r + l) / 2);
+    if (arr[i] === value) {
+      return i;
+    } else if (arr[i] > value) {
+      r = i - 1;
+    } else {
+      l = i;
+    }
+  }
+  if (arr[l] === value) return l;
 }
